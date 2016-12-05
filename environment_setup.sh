fff8d8d83b95be9fdb18d1fb8dd79c0a5f5ca918
Laszlo <visualjazz@gmail.com>
2016-12-05 17:55:38 -0600

    Adds header comment to environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,1 +1,12 @@
-#Update contents
+#!/bin/bash       
+#title           :environment_setup.sh
+#description     :This script will clone a git project, based on user selection.
+#author		 :A. Laszlo Lazuer
+#date            :12/05/2016
+#version         :0.1a
+#usage		 :./environment_setup.sh
+#notes           :Execute from within a UNIX/Linux terminal.
+#bash_version    :3.2.57(1)
+#===============================================================================
+
+Echo "Menu here"
