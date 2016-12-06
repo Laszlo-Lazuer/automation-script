6c663d8b881cca39a25de7026ad4341dcd5abba9
Laszlo <visualjazz@gmail.com>
2016-12-06 14:39:35 -0600

    Updated script header to reflect beta version

--- environment_setup.sh
+++ environment_setup.sh
@@ -3,7 +3,7 @@
 #description     :This script will clone a git project, based on user selection.
 #author		 :A. Laszlo Lazuer
 #date            :12/05/2016
-#version         :0.1a
+#version         :1.0beta
 #usage		 :./environment_setup.sh
 #notes           :Execute from within a UNIX/Linux terminal.
 #bash_version    :3.2.57(1)
